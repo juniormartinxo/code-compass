842de3773bb4e9b1559125a3c4a501c6956b1957
Junior Martins <amjr.box@gmail.com>
2026-02-10 11:06:01 -0300

    docs(comandos): adicione arquivo _meta para indexador

---
+++ apps/docs/pages/indexer/commands/_meta.js
@@ -0,0 +1,8 @@
+export default {
+  init: 'init - Inicializar',
+  scan: 'scan - Escanear',
+  chunk: 'chunk - Chunking',
+  index: 'index - Indexar',
+  search: 'search - Buscar',
+  ask: 'ask - Perguntar',
+};
